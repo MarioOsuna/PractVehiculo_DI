70b9d72bdba79bd10ce76c13fb777343d80c0dd5
MarioOsuna <marioosuna26@gmail.com>
2019-12-06 11:53:34 +0100

    Coches.java realizado, se debe comprobar

--- app/src/main/java/com/example/vehiculos_di/Coches.java
+++ app/src/main/java/com/example/vehiculos_di/Coches.java
@@ -1,32 +1,57 @@
 package com.example.vehiculos_di;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
 
 public class Coches extends AppCompatActivity {
     Button buttonVolver, buttonModificar, buttonInsertar, buttonBorrar, buttonMostrar;
     EditText matricula, marca, color;
-    ListView listaCoche;
+    ListView lista;
+    static String SERVIDOR = "http://192.168.100.19";//casa
+    //static String SERVIDOR = "http://192.168.0.111:8080";//clase
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.coches);
         buttonVolver = findViewById(R.id.buttonVolver);
+
         buttonModificar = findViewById(R.id.buttonModCoches);
         buttonInsertar = findViewById(R.id.buttonInsCoches);
         buttonBorrar = findViewById(R.id.buttonBorCoches);
         buttonMostrar = findViewById(R.id.buttonMosCoches);
+
         matricula = findViewById(R.id.editTextMatrCoches);
         marca = findViewById(R.id.editTextMarca);
         color = findViewById(R.id.editTextColor);
-        listaCoche = findViewById(R.id.ListaCoches);
+        lista = findViewById(R.id.ListaCoches);
+
         buttonVolver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,7 +61,270 @@ public class Coches extends AppCompatActivity {
                 finish();
             }
         });
+        buttonMostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DescargarCSV descargarCSV = new DescargarCSV();
+                descargarCSV.execute("/web/listadoCSV.php");
+                //descargarCSV.execute("/pract/listadoCSV.php");
+            }
+        });
+        buttonInsertar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (matricula.getText().toString() == " ") {
+                    Toast.makeText(Coches.this, "Debe introducir al menos el campo matrícula", Toast.LENGTH_SHORT).show();
+                } else {
+                    String dir2 = "/web/listadoCSV.php";
+                    if (!comprobar(matricula.getText().toString(), dir2)) {
+                        String dir = "/web/insertarPOSTCoches.php";
+                        Insertar(matricula.getText().toString(), marca.getText().toString(), color.getText().toString(), dir);
+                    } else {
+                        Toast.makeText(Coches.this, "Ya se ha introducido esa matrícula", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        buttonModificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (matricula.getText().toString() == " ") {
+                    Toast.makeText(Coches.this, "Debe introducir al menos el campo matrícula", Toast.LENGTH_SHORT).show();
+                } else {
+                    String dir2 = "/web/listadoCSV.php";
+                    if (comprobar(matricula.getText().toString(), dir2)) {
+                        String dir = "/web/updateGETCoches.php";
+                        Modificar(matricula.getText().toString(), marca.getText().toString(), color.getText().toString(), dir);
+                    } else {
+                        Toast.makeText(Coches.this, "No existe esa matrícula", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+        buttonBorrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (matricula.getText().toString() == " ") {
+                    Toast.makeText(Coches.this, "Debe introducir al menos el campo matrícula", Toast.LENGTH_SHORT).show();
+                } else {
+                    String dir2 = "/web/listadoCSV.php";
+
+                    if (comprobar(matricula.getText().toString(), dir2)) {
+                        String dir = "/web/deleteGET.php";
+                        Eliminar(matricula.getText().toString(), dir);
+                    } else {
+                        Toast.makeText(Coches.this, "No existe esa matrícula", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
 
 
     }
+
+    private class DescargarCSV extends AsyncTask<String, Void, Void> {
+        String total = "";
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(Coches.this);
+            progressDialog.setIndeterminate(true);
+            progressDialog.setTitle("Descargando la información de la red en csv.");
+            progressDialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            ArrayAdapter<String> adapter;
+            List<String> list = new ArrayList<String>();
+
+            String[] lineas = total.split("\n");
+
+            for (String lin : lineas) {
+                String[] campos = lin.split(",");
+                String dato = " MATRÍCULA: " + campos[0];
+                dato += " MARCA: " + campos[1];
+                dato += " COLOR: " + campos[2];
+                list.add(dato);
+            }
+            adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item, list);
+            lista.setAdapter(adapter);
+
+            progressDialog.dismiss();
+        }
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            String script = strings[0];
+
+            URL url = null;
+            HttpURLConnection httpURLConnection = null;
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                url = new URL(SERVIDOR + script);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.connect();
+
+                if (httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    InputStream inputStream = httpURLConnection.getInputStream();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+
+                    String linea = "";
+
+                    while ((linea = br.readLine()) != null) {
+                        total += linea + "\n";
+                    }
+
+                    br.close();
+                    inputStream.close();
+
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Log.i("CONEXION", total);
+
+            return null;
+        }
+    }
+
+    private void Insertar(String Matricula, String Marca, String color, String dir) {
+        String script = null;
+
+        script = SERVIDOR + dir;
+
+        String contenido = "";
+        try {
+            System.out.println(script);
+            URLConnection conexion = null;
+
+            conexion = new URL(script).openConnection();
+            //conexion.connect();
+            conexion.setDoOutput(true);
+
+            PrintStream ps = new PrintStream(conexion.getOutputStream());
+
+            ps.print("Matricula=" + Matricula);
+            ps.print("&Marca=" + Marca);
+            ps.print("&color=" + color);
+
+            InputStream inputStream = conexion.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+            String linea = "";
+
+            while ((linea = br.readLine()) != null) {
+                contenido += linea;
+            }
+
+            br.close();
+
+        } catch (MalformedURLException ex) {
+        } catch (IOException e) {
+        }
+
+    }
+
+    private void Modificar(String Matricula, String marca, String color, String dir) {
+        String script = SERVIDOR + dir + "?Matricula=" + Matricula + "&Marca=" + marca + "&Color=" + color;
+        String contenido = "";
+
+        System.out.println(script);
+        try {
+
+            URLConnection conexion = null;
+
+            conexion = new URL(script).openConnection();
+            conexion.connect();
+            InputStream inputStream = conexion.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+
+            String linea = "";
+
+            while ((linea = br.readLine()) != null) {
+                contenido += linea;
+
+            }
+            br.close();
+
+        } catch (MalformedURLException ex) {
+
+        } catch (UnsupportedEncodingException ex) {
+        } catch (IOException ex) {
+
+        }
+    }
+
+    private void Eliminar(String matricula, String dir) {
+        String script = SERVIDOR + dir + "?matricula=" + matricula;
+        String contenido = "";
+        try {
+
+            URLConnection conexion = null;
+
+            conexion = new URL(script).openConnection();
+            conexion.connect();
+            InputStream inputStream = conexion.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+
+            String linea = "";
+
+            while ((linea = br.readLine()) != null) {
+                contenido += linea;
+
+            }
+            br.close();
+
+        } catch (MalformedURLException ex) {
+        } catch (UnsupportedEncodingException ex) {
+        } catch (IOException ex) {
+        }
+    }
+
+    private boolean comprobar(String cadena, String dir) {
+        boolean existe = false;
+        String csv = "";
+        String scriptConsulta = SERVIDOR + dir;
+
+        URLConnection conexion = null;
+        try {
+            conexion = new URL(scriptConsulta).openConnection();
+
+            InputStream inputStream = conexion.getInputStream();
+
+            BufferedReader bf = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+
+            String linea = "";
+
+            while ((linea = bf.readLine()) != null) {
+                csv += linea + "\n";
+            }
+            bf.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        String contenido = csv;
+
+        String lineas[] = contenido.split("\n");
+
+        for (String lin : lineas) {
+            String[] campos = lin.split(",");
+            if (cadena.equals(campos[0])) {
+                existe = true;
+            }
+        }
+        return existe;
+
+    }
+
 }
--- app/src/main/java/com/example/vehiculos_di/Gastos.java
+++ app/src/main/java/com/example/vehiculos_di/Gastos.java
@@ -1,8 +1,11 @@
 package com.example.vehiculos_di;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -10,10 +13,31 @@ import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Gastos extends AppCompatActivity {
     Button buttonVolver, buttonModificar, buttonInsertar, buttonBorrar, buttonMostrar;
     EditText matricula, concepto, valor, id;
-    ListView listaGastos;
+    ListView lista;
+    static String SERVIDOR = "http://192.168.100.19";
+    //static String SERVIDOR = "http://192.168.0.111:8080";//Clase
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +52,9 @@ public class Gastos extends AppCompatActivity {
         concepto = findViewById(R.id.editTextConcepto);
         valor = findViewById(R.id.editTextValor);
         id = findViewById(R.id.editTextID);
-        listaGastos = findViewById(R.id.ListaGastos);
+        lista = findViewById(R.id.ListaGastos);
 
-        /*buttonVolver.setOnClickListener(new View.OnClickListener() {
+        buttonVolver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
@@ -38,7 +62,120 @@ public class Gastos extends AppCompatActivity {
                 setResult(RESULT_OK, intent);
                 finish();
             }
-        });*/
+        });
+        buttonMostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DescargarJSON descargarJSON = new DescargarJSON();
+                descargarJSON.execute("/web/listadoJSON.php");
+                //descargarJSON.execute("/pract/listadoJSON.php");
+            }
+        });
+        buttonInsertar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        buttonModificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        buttonBorrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    private class DescargarJSON extends AsyncTask<String, Void, Void> {
+        List<String> list = new ArrayList<String>();
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            String script = strings[0];
+            String url = SERVIDOR + script;
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            String contenido = "";
+            try {
+
+                URLConnection conexion = null;
+
+                conexion = new URL(url).openConnection();
+                conexion.connect();
+                InputStream inputStream = conexion.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+
+                String linea = "";
+
+                while ((linea = br.readLine()) != null) {
+                    contenido += linea;
+
+                }
+                br.close();
+
+            } catch (MalformedURLException ex) {
+            } catch (UnsupportedEncodingException ex) {
+            } catch (IOException ex) {
+            }
+            JsonParser parser = new JsonParser();
+            JsonArray jsonArray = parser.parse(contenido).getAsJsonArray();
+            String fila1 = "ID\t\t\tMatrícula\t\t\tConcepto\t\t\t\t\tValor";
+            list.add(fila1);
+            for (JsonElement elemento : jsonArray) {
+                String fila = "";
+                JsonObject objeto = elemento.getAsJsonObject();
+
+
+                Set<Map.Entry<String, JsonElement>> entrySet = objeto.entrySet();
+                int contador = 0;
+
+                for (Map.Entry<String, JsonElement> entry : entrySet) {
+                    if (contador % 2 != 0) {
+                        fila += "  " + entry.getValue().getAsString();
+                    }
+                    contador++;
+                    // entry.getValue();
+
+                }
+
+                list.add(fila);
+
+
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(Gastos.this);
+            progressDialog.setIndeterminate(true);
+            progressDialog.setTitle("Descargando la información de la red en JSON.");
+            progressDialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            ArrayAdapter<String> adapter;
+
+            adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item, list);
+            lista.setAdapter(adapter);
+
+            progressDialog.dismiss();
+        }
     }
 
 }
